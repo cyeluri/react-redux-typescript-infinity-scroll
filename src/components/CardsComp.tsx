b56930eb2946c80fa511f892085400335255c757
Chaitanya Yeluri <chaitanya.yeluri@vertivco.com>
2019-12-16 16:48:07 -0500

    CARDS-APP# - cards SEARCH - including search component into cards.

--- src/components/CardsComp.tsx
+++ src/components/CardsComp.tsx
@@ -3,8 +3,9 @@ import * as React from 'react';
 // connect component to redux-store
 import {connect} from 'react-redux';
 
-// import the postActions
+// import the cardActions
 import {fetchCards} from '../redux/actions/CardsActions';
+import CardsSearchComp from './CardsSearchComp';
 
 // store will come from the applicaiton state 
 
@@ -20,7 +21,7 @@ interface State {
 
 
 class CardsComp extends React.Component<Props, State> {
-    
+
     constructor(props:Props) {
         super(props);
     }
@@ -33,12 +34,19 @@ class CardsComp extends React.Component<Props, State> {
         const cardsList = this.props.cardsState.map(
             (card:any) => (
                 <div key={card.id}>
-                    <h3>{card.name}</h3>
+                    <p>
+                        <h3>Name :{card.name}</h3>
+                        <h3>Artist :{card.artist}</h3>
+                        <h3>Set Name :{card.setName}</h3>
+                        <h3>Original type :{card.originalType}</h3>
+                    </p>
+                    <img src={card.imageUrl}/>
                 </div>
             )
         );
         return (
         <div>
+            <CardsSearchComp />
             <h1>Cards</h1>
             {cardsList}
         </div>
@@ -48,7 +56,7 @@ class CardsComp extends React.Component<Props, State> {
 }
 
 const mapStateToProps = (state: any) => ({
-    cardsState: state.cardsReducer.payload
+    cardsState: state.cardsReducer.cardsPayload
 });
 
 //export default Cards;
