c5ff0b810c6048e67f54252d37335c3334d32c4b
Chaitanya Yeluri <chaitanya.yeluri@vertivco.com>
2020-01-10 16:11:35 -0500

    CARDS-APP# - QUALITY# - fixing the runtime error if the browser doesn't have redux devtools installed.

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,22 +1,20 @@
 import {createStore, applyMiddleware, compose} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import allReducers from './reducers';
+
 
 const initialState:any = {};
 
 const middleware  = [thunk];
 
-const composeEnhancers =
-    typeof window === 'object' &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-const store = createStore(allReducers, 
-                initialState, 
-                compose(
-                    applyMiddleware(...middleware),
-                    composeEnhancers
-                )
-    );
+const store = createStore(
+    allReducers,
+    initialState,
+    composeWithDevTools(
+        applyMiddleware(...middleware)
+        )
+    )
+;
 
 export default store;
